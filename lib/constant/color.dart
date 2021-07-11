0b890a85e72c92aed558de9117117f142d29a366
amalkhairin <amallanch45@gmail.com>
2021-07-11 18:22:23 +0700

    add color palette

---
+++ lib/constant/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF4137E5);
+const Color secondaryColor = Color(0xFFFFFFFF);
+const Color fillColor = Color(0xFFF0F0F0);
+const Color itemBackgroundColor = Color(0xFFECF8F6);
