d6e4d7b8f268e2e5adf47e334d37334225dccec9
amalkhairin <amallanch45@gmail.com>
2021-07-13 08:41:28 +0700

    add app bottom navigation bar

---
+++ lib/pages/main_page/app_navigation.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:quantri_app/constant/color.dart';
+import 'package:quantri_app/pages/main_page/dashboard/dashboard_main_page.dart';
+
+class AppNavigation extends StatefulWidget {
+  const AppNavigation({ Key? key }) : super(key: key);
+
+  @override
+  _AppNavigationState createState() => _AppNavigationState();
+}
+
+class _AppNavigationState extends State<AppNavigation> {
+
+  int _selectedPage = 0;
+
+  List<Widget> _pages = [
+    DashboardPage(),
+    Container(),
+    Container(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _pages[_selectedPage],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
+        backgroundColor: Colors.white,
+        currentIndex: _selectedPage,
+        selectedItemColor: primaryColor,
+        unselectedItemColor: Colors.grey[400],
+        elevation: 0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 24,),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_rounded, size: 36, color: Colors.red,),
+            label: "Add"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded, size: 24,),
+            label: "Profile"
+          ),
+        ],
+      ),
+    );
+  }
+}
