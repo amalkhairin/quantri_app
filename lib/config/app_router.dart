66cb856b4528ad5df07dda87a0f8a3bd3e262cb1
amalkhairin <amallanch45@gmail.com>
2021-07-14 15:04:10 +0700

    update app router

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -3,6 +3,7 @@ import 'package:quantri_app/pages/login/login_page.dart';
 import 'package:quantri_app/constant/router_name.dart';
 import 'package:quantri_app/pages/main_page/app_navigation.dart';
 import 'package:quantri_app/pages/main_page/dashboard/dashboard_main_page.dart';
+import 'package:quantri_app/pages/main_page/dashboard/tenant/tenant_category_detail_page.dart';
 import 'package:quantri_app/pages/register/register_step_one_page.dart';
 import 'package:quantri_app/pages/register/register_step_two_page.dart';
 
@@ -16,6 +17,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => RegisterStepTwoPage(data: settings.arguments,));
     case APP_NAVIGATION_ROUTE:
       return MaterialPageRoute(builder: (context) => AppNavigation());
+    case TENANT_CATEGORY_DETAIL_ROUTE:
+      return MaterialPageRoute(builder: (context) => TenantCategoryDetailPage(categoryName: settings.arguments,));
     default:
       return MaterialPageRoute(builder: (context) => LoginPage());
   }
