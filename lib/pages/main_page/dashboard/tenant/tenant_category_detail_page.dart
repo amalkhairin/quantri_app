4062395c1f25c50c9303ac44753059612e6611fa
amalkhairin <amallanch45@gmail.com>
2021-08-23 15:34:59 +0700

    add distance between current position and tenant position

--- lib/pages/main_page/dashboard/tenant/tenant_category_detail_page.dart
+++ lib/pages/main_page/dashboard/tenant/tenant_category_detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:quantri_app/component/input_form/qa_search_form.dart';
 import 'package:quantri_app/constant/color.dart';
 import 'package:quantri_app/constant/router_name.dart';
+import 'package:quantri_app/models/tenant_model.dart';
 
 class TenantCategoryDetailPage extends StatefulWidget {
   final String? categoryName;
@@ -16,6 +18,42 @@ class _TenantCategoryDetailPageState extends State<TenantCategoryDetailPage> {
 
   // search text controller
   TextEditingController _searchController = TextEditingController();
+  Position? _myPosition;
+  List<Tenant> _listTenant = [];
+
+  List<List<double>> _latlong = [
+    [-7.025253, 107.519760],
+    [-7.374585, 108.558189],
+    [-6.571589, 107.758736],
+    [-6.953946, 107.677765],
+    [-6.939008, 107.740753],
+    [-6.595038, 106.816635],
+    [-6.905977, 107.613144],
+    [-6.923700, 106.928726],
+  ];
+
+  Future<void> _getMyPosition() async {
+    Position pos = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    print(pos);
+    setState(() {
+      _myPosition = pos;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getMyPosition();
+    _listTenant = List.generate(8, (index) => Tenant(
+      tenantID: index+1,
+      tenantName: "Pemkot Bandung ${index+1}",
+      latitude: _latlong[index][0],
+      longitude: _latlong[index][1],
+      address: "Jl. Sangkuriang No.10A, Dago, Kecamatan Coblong, Kota Bandung, Jawa Barat",
+      description: "Dinas Catatan Sipil dan Kependudukan",
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,13 +89,13 @@ class _TenantCategoryDetailPageState extends State<TenantCategoryDetailPage> {
           height: screenSize.height,
           child: ListView.builder(
             physics: BouncingScrollPhysics(),
-            itemCount: 7,
+            itemCount: _listTenant.length,
             itemBuilder: (context, index){
               return Padding(
                 padding: const EdgeInsets.fromLTRB(24, 14, 24, 14),
                 child: InkWell(
                   onTap: (){
-                    Navigator.of(context).pushNamed(TENANT_DETAIL_ROUTE, arguments: "Pemkot Bandung");
+                    Navigator.of(context).pushNamed(TENANT_DETAIL_ROUTE, arguments: _listTenant[index].tenantName);
                   },
                   child: Ink(
                     width: screenSize.width,
@@ -76,11 +114,20 @@ class _TenantCategoryDetailPageState extends State<TenantCategoryDetailPage> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("Pemkot Bandung", style: TextStyle(fontWeight: FontWeight.bold),),
+                              Text(_listTenant[index].tenantName!, style: TextStyle(fontWeight: FontWeight.bold),),
                               SizedBox(height: 8,),
-                              Text("Dinas Catatan Sipil dan Kependudukan"),
+                              Text(_listTenant[index].description!),
                               SizedBox(height: 8,),
-                              Text("Jl. Sangkuriang No.10A, Dago, Kecamatan Coblong, Kota Bandung, Jawa Barat", maxLines: 3, overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.grey[600]),),
+                              Text(_listTenant[index].address!, maxLines: 3, overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.grey[600]),),
+                              SizedBox(height: 10,),
+                              _myPosition == null
+                              ? Text("...")
+                              : Row(
+                                children: [
+                                  Icon(Icons.location_on, color: Colors.red, size: 14,),
+                                  Text("${(Geolocator.distanceBetween(_myPosition!.latitude, _myPosition!.longitude, _listTenant[index].latitude!, _listTenant[index].longitude!)/10000).toStringAsFixed(2)} Km"),
+                                ],
+                              ),
                             ],
                           ),
                         )
